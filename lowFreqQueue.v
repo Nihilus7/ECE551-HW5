f723d1c42adfa71f83958309b6df52a6f6135981
Nihilus7 <SWiener@wisc.edu>
2015-11-18 17:47:31 -0600

    Update lowFreqQueue.v

--- lowFreqQueue.v
+++ lowFreqQueue.v
@@ -1,12 +1,12 @@
 module lowFreqQueue(clk, rst_n, new_smpl, wrt_smpl, smpl_out, sequencing);
-input clk, rst_n
+input clk, rst_n;
 input [15:0] new_smpl;
 input wrt_smpl;
 
 output [15:0] smpl_out;
 output sequencing;
 
-logic full, read, write;
+logic read, write, set_read_ptr;
 
 typedef enum reg {IDLE, READ} state_t;
 state_t state, nxt_state;
@@ -25,7 +25,7 @@ dualPort1024x16 idualPort1024x16(.clk(clk), .we(write), .waddr(new_ptr), .raddr(
 // State Register
 always_ff @(posedge clk, negedge rst_n)
 	if (!rst_n)
-		state <= INIT;
+		state <= IDLE;
 	else
 		state <= nxt_state;
 
